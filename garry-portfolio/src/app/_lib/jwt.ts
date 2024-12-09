d06b546fdcf75f833459c43c7d656ed3c35063c1
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-12-09 11:04:00 +0900

    feat: 리프레시 토큰 검증 로직 추가

--- garry-portfolio/src/app/_lib/jwt.ts
+++ garry-portfolio/src/app/_lib/jwt.ts
@@ -1,10 +1,13 @@
 import jwt from 'jsonwebtoken'
+import { findRefreshToken } from './dbToken'
 
 const JWT_SECRET = process.env.JWT_SECRET as string
 
 interface TokenPayload {
   userId: number
   email: string
+  iat?: number // 생성 시간
+  exp?: number // 만료 시간
 }
 
 interface TokenWithExpiry {
@@ -28,6 +31,36 @@ export function generateRefreshToken(payload: TokenPayload): TokenWithExpiry {
   return { token, expiresAt }
 }
 
-export function verifyToken(token: string) {
-  return jwt.verify(token, JWT_SECRET)
+export function verifyToken(token: string): TokenPayload {
+  const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload
+
+  // 타입 가드
+  if (typeof decoded !== 'object' || decoded === null) {
+    throw new Error('TokenPayload가 유효하지 않습니다.')
+  }
+
+  if (typeof decoded.userId !== 'number' || typeof decoded.email !== 'string') {
+    throw new Error('TokenPayload 형식 오류 (userId || string)')
+  }
+
+  return decoded
+}
+
+export async function validateRefreshToken(refreshToken: string): Promise<TokenPayload> {
+  // Refresh Token 검증
+  const decoded = verifyToken(refreshToken)
+  console.log('decoded: ', decoded)
+
+  // Refresh Token DB 저장 여부 확인
+  const storedToken = await findRefreshToken(refreshToken)
+  if (!storedToken) {
+    throw new Error('유효하지 않은 Refresh Token입니다.')
+  }
+
+  // Refresh Token 만료 여부 확인
+  if (new Date() > storedToken.expiresAt) {
+    throw new Error('Refresh Token이 만료되었습니다.')
+  }
+
+  return decoded
 }
