5ccee6beed02f14434dbebaf0e57353bf15dc48e
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-12-13 23:07:25 +0900

    feat: 클라이언트 api 요청 axios 추가

---
+++ garry-portfolio/src/app/services/apiClient.ts
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+const apiAuthClient = axios.create({
+  ...apiClient.defaults,
+})
+
+// 요청 인터셉터
+apiAuthClient.interceptors.request.use((config) => {
+  // 액세스 토큰 가져오기
+  const accessToken = localStorage.getItem('access-token')
+
+  //   액세스 토큰 존재시 header에 추가
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
+
+  return config
+})
+
+// 응답 인터셉터
+apiAuthClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error)
+  }
+)
