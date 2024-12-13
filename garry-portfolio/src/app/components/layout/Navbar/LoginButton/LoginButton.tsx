e604a2da1d76f51efe1ddc4534676ea9d03d5e97
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-12-13 15:38:47 +0900

    feat: 로그인 버튼 수정

--- garry-portfolio/src/app/components/layout/Navbar/LoginButton/LoginButton.tsx
+++ garry-portfolio/src/app/components/layout/Navbar/LoginButton/LoginButton.tsx
@@ -2,17 +2,52 @@
 
 import Button from '@/app/components/Button/Button'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const LoginButton: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const onClickLogout = () => {}
+  // 컴포넌트 로드시 로그인 여부 검사
+  useEffect(() => {
+    const isAccessTokenSaved = checkAccessToken()
+
+    if (isAccessTokenSaved) {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const onClickLogout = () => {
+    removeAccesTokenFromLocalStorage()
+  }
+
+  const checkAccessToken = () => {
+    // 로컬 스토리지에서 액세스 토큰 존재 여부 검사
+    const accessToken = localStorage.getItem('access-token')
+
+    if (!accessToken) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  // 로컬 스토리지에서 액세스 토큰 삭제
+  const removeAccesTokenFromLocalStorage = () => {
+    // 로컬 스토리지에서 액세스 토큰 존재 여부 검사
+    checkAccessToken()
+
+    localStorage.removeItem('access-token')
+
+    // 로그아웃
+    setIsLoggedIn(false)
+  }
+
+  // 쿠키 삭제 차후 구현
 
   return (
     <>
       {isLoggedIn ? (
-        <Button>로그아웃</Button>
+        <Button onClick={onClickLogout}>로그아웃</Button>
       ) : (
         <Link href={'/api/auth/login'}>
           <Button>로그인</Button>
