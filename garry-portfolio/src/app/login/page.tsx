f77c8cd9febc69615b530e77b9b8e46e9dfaaf71
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-12-13 14:15:57 +0900

    feat: 액세스 토큰 저장 후 메인 페이지로 이동 로직 추가

--- garry-portfolio/src/app/login/page.tsx
+++ garry-portfolio/src/app/login/page.tsx
@@ -6,11 +6,19 @@ import { useEffect } from 'react'
 const Login: React.FC = () => {
   const searchParams = useSearchParams()
 
+  const saveAccessTokenAtLocalStorage = (accessToken: string) => {
+    if (!accessToken) return
+
+    localStorage.setItem('access-token', accessToken)
+  }
+
   // 컴포넌트(페이지) 렌더링시 실행
   useEffect(() => {
     const accessToken = searchParams.get('access-token')
     if (accessToken) {
-      console.log('액세스 토큰 값: ', accessToken)
+      saveAccessTokenAtLocalStorage(accessToken)
+
+      window.location.href = '/'
     } else {
       console.log('액세스 토큰이 없습니다.')
     }
