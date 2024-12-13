12c506e87e61ebeb89893aa83950f941026e8dea
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-12-13 15:38:38 +0900

    feat: 로그인 리다이렉트 페이지 로그인 로직 구현

--- garry-portfolio/src/app/login/page.tsx
+++ garry-portfolio/src/app/login/page.tsx
@@ -6,7 +6,9 @@ import { useEffect } from 'react'
 const Login: React.FC = () => {
   const searchParams = useSearchParams()
 
+  // 액세스 토큰 로컬 스토리지에 저장
   const saveAccessTokenAtLocalStorage = (accessToken: string) => {
+    // 액세스 토큰 검사
     if (!accessToken) return
 
     localStorage.setItem('access-token', accessToken)
@@ -14,6 +16,7 @@ const Login: React.FC = () => {
 
   // 컴포넌트(페이지) 렌더링시 실행
   useEffect(() => {
+    // 쿼리 파라미터에서 액세스 토큰 받아오기
     const accessToken = searchParams.get('access-token')
     if (accessToken) {
       saveAccessTokenAtLocalStorage(accessToken)
@@ -22,7 +25,7 @@ const Login: React.FC = () => {
     } else {
       console.log('액세스 토큰이 없습니다.')
     }
-  })
+  }, [])
 
   return (
     <>
