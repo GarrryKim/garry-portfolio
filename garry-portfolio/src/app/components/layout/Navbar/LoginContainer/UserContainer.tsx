dc1c01417fa2943d1930f5397a275f3e5d215566
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2025-01-07 18:00:50 +0900

    feat: 로그인 버튼 함수 변경

--- garry-portfolio/src/app/components/layout/Navbar/LoginContainer/UserContainer.tsx
+++ garry-portfolio/src/app/components/layout/Navbar/LoginContainer/UserContainer.tsx
@@ -18,8 +18,15 @@ const UserContainer: React.FC = () => {
     }
   }, [])
 
-  const onClickLogout = () => {
+  const handleLogin = () => {
+    window.location.href = '/api/auth/login'
+  }
+
+  const handleLogout = () => {
     removeAccesTokenFromLocalStorage()
+
+    // 로그아웃
+    setIsLoggedIn(false)
   }
 
   const checkAccessToken = () => {
@@ -39,9 +46,6 @@ const UserContainer: React.FC = () => {
     checkAccessToken()
 
     localStorage.removeItem('access-token')
-
-    // 로그아웃
-    setIsLoggedIn(false)
   }
 
   // 쿠키 삭제 차후 구현
@@ -51,12 +55,10 @@ const UserContainer: React.FC = () => {
       {isLoggedIn ? (
         <>
           <UserInfo />
-          <Button onClick={onClickLogout}>로그아웃</Button>
+          <Button onClick={handleLogout}>로그아웃</Button>
         </>
       ) : (
-        <Link href={'/api/auth/login'}>
-          <Button>로그인</Button>
-        </Link>
+        <Button onClick={handleLogin}>로그인</Button>
       )}
     </div>
   )
