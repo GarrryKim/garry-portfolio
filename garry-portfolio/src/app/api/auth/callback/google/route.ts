2619b0c2c48f50c7431e4366ea9931bf5c0993f4
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2025-01-10 14:04:35 +0900

    refactor: 구글 OAuth 로그인 과정 서비스 로직 분리

--- garry-portfolio/src/app/api/auth/callback/google/route.ts
+++ garry-portfolio/src/app/api/auth/callback/google/route.ts
@@ -1,11 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
-import { generateAccessToken, generateRefreshToken } from '@/app/_lib/jwt'
-import { exchangeCodeForToken, fetchGoogleUserInfo, GoogleUserInfo } from '@/app/_lib/oauth'
 import { DatabaseError, OAuthError } from '@/app/_lib/errors'
-import { User } from '@/types/user'
-import { createUser, getUserByEmail } from '@/services/userService'
-import { saveRefreshToken } from '@/services/jwtService'
+import { handleGoogleOAuth, validateState } from '@/services/authService'
 
 /**
  * @swagger
@@ -34,48 +30,17 @@ export async function GET(request: NextRequest) {
   const cookieStore = await cookies()
   const storedState = cookieStore.get('oauth_state')?.value
 
-  //   state값 검증
-  if (state !== storedState) {
-    return NextResponse.json({ error: '유효하지 않은 state 입니다.' }, { status: 400 })
-  }
-
-  // state값 검증 후 삭제
-  cookieStore.delete('oauth_state')
-
   try {
-    // 1. Authroization Code를 받아서 Access Token으로 교환
-    const tokenData = await exchangeCodeForToken(code)
-    const googleAccessToken = tokenData.access_token
+    // state 값 검증
+    await validateState(state, storedState)
 
-    // 2. AccessToken으로 사용자 정보 받아오기
-    const userInfo: GoogleUserInfo = await fetchGoogleUserInfo(googleAccessToken)
-    const { email, name, picture } = userInfo
+    // state값 검증 후 삭제
+    cookieStore.delete('oauth_state')
 
-    // 이메일 검증
-    if (!email) {
-      return NextResponse.json({ error: '유효하지 않은 이메일입니다.' }, { status: 400 })
-    }
+    // Google OAuth 처리
+    const { accessToken, refreshToken } = await handleGoogleOAuth(code)
 
-    // 3. DB에서 사용자 조회
-    let user: User | null = await getUserByEmail(email)
-
-    // 4. 사용자가 없다면 생성(회원가입)
-    if (!user) {
-      user = await createUser(email, name, picture)
-    }
-
-    // 5. JWT 토큰 생성
-    const accessToken = generateAccessToken({ userId: user.id, email: user.email })
-    const { token: refreshToken, expiresAt } = generateRefreshToken({ userId: user.id, email: user.email })
-
-    // 6. Refresh Token을 DB에 저장
-    await saveRefreshToken({
-      userId: user.id,
-      refreshToken: refreshToken,
-      expiresAt: expiresAt,
-    })
-
-    // 7. Access Token을 리디렉션 URL에 포함
+    // Redirect URL 생성
     if (!process.env.JWT_REDIRECT_URL) {
       throw new Error('JWT_REDIRECT_URL 환경 변수가 설정되지 않았습니다.')
     }
@@ -94,7 +59,7 @@ export async function GET(request: NextRequest) {
 
     return response
   } catch (error) {
-    console.error('인증 혹은 DB 오류: ', error)
+    console.error('Google OAuth 에러: ', error)
 
     if (error instanceof OAuthError) {
       return NextResponse.json({ error: error.message }, { status: 400 })
--- garry-portfolio/src/services/authService.ts
+++ garry-portfolio/src/services/authService.ts
@@ -1,4 +1,9 @@
+import { OAuthError } from '@/app/_lib/errors'
 import { randomUUID } from 'crypto'
+import { generateAccessToken, generateRefreshToken } from '@/app/_lib/jwt'
+import { exchangeCodeForToken, fetchGoogleUserInfo, GoogleUserInfo } from '@/app/_lib/oauth'
+import { createUser, getUserByEmail } from '@/services/userService'
+import { saveRefreshToken } from '@/services/jwtService'
 
 export async function createAuthUrl() {
   const clientId = process.env.GOOGLE_CLIENT_ID!
@@ -12,4 +17,47 @@ export async function createAuthUrl() {
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&state=${state}`
 
   return { state, authUrl }
+}
+
+export async function validateState(state: string, storedState: string | undefined) {
+  //   state값 검증
+  if (!storedState || state !== storedState) {
+    throw new OAuthError('유효하지 않은 토큰입니다.')
+  }
+}
+
+export async function handleGoogleOAuth(code: string): Promise<{ accessToken: string; refreshToken: string }> {
+  // 1. Authroization Code를 받아서 Access Token으로 교환
+  const tokenData = await exchangeCodeForToken(code)
+  const googleAccessToken = tokenData.access_token
+
+  // 2. AccessToken으로 사용자 정보 받아오기
+  const userInfo: GoogleUserInfo = await fetchGoogleUserInfo(googleAccessToken)
+  const { email, name, picture } = userInfo
+
+  // 이메일 검증
+  if (!email) {
+    throw new OAuthError('유효하지 않은 이메일입니다.')
+  }
+
+  // 3. DB에서 사용자 조회
+  let user = await getUserByEmail(email)
+
+  // 4. 사용자가 없다면 생성(회원가입)
+  if (!user) {
+    user = await createUser(email, name, picture)
+  }
+
+  // 5. JWT 토큰 생성
+  const accessToken = generateAccessToken({ userId: user.id, email: user.email })
+  const { token: refreshToken, expiresAt } = generateRefreshToken({ userId: user.id, email: user.email })
+
+  // 6. Refresh Token을 DB에 저장
+  await saveRefreshToken({
+    userId: user.id,
+    refreshToken: refreshToken,
+    expiresAt: expiresAt,
+  })
+
+  return { accessToken, refreshToken }
 }
